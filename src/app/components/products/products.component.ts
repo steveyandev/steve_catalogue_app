cf8a9583dfb676b7c8e9b45309267cf001f3c5fe
steve desire <stevedesiredev@gmail.com>
2023-11-06 00:24:22 +0100

    creation and implementation of the method that used the service to get product.Html and typescript code to show product in our application. product visible on the component and our interface (all product implementation)

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ProductsService} from "../../services/products.service";
+import {Product} from "../../model/product.model";
 
 @Component({
   selector: 'app-products',
@@ -7,8 +8,15 @@ import {ProductsService} from "../../services/products.service";
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit{
+  //? pour dire optionel ou utiliser =null
+  products:Product[]|null=null;
   constructor(private productService:ProductsService) { }
   ngOnInit(): void {
   }
 
+  onGetAllProducts() {
+    this.productService.getAllProducts().subscribe(data=>{
+      this.products=data;
+    })
+  }
 }
